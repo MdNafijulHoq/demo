08251d96dfe9592aeb498b188ccde85e22ab00dd
MdNafijulHoq <nafijulhaque118@gmail.com>
2025-06-16 08:54:15 +0600

    login page done

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log("Server is running on port 3000");
 console.log("Use the following command to start the server: node server/index.js");
+console.log("Working on login page and user authentication");
+
